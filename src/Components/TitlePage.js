7c69f5d8c0ddc031730cb939fa564ba15b307540
Guruprasad <gp04431@gmail.com>
2025-01-05 14:41:37 +0530

    Adding GMaps

--- src/Components/TitlePage.js
+++ src/Components/TitlePage.js
@@ -4,11 +4,10 @@ import "../Styles/TitlePage.css";
 import { FaLinkedinIn } from "react-icons/fa6";
 import { IoLogoInstagram } from "react-icons/io5";
 import { SiGmail } from "react-icons/si";
-import Logo from '../Logo.png';
+import Logo from "../Logo_bg.png";
 const ContactCard = ({ platform }) => {
   return (
     <div className="contact-container">
-     
       <a
         href={`https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(
           platform[1].url
@@ -107,8 +106,7 @@ function TitlePage() {
   }, []);
   return (
     <div className="coming-soon-container">
-      <div className="circular-gradient">
-      </div>
+      <div className="circular-gradient"></div>
       <div className="circular-gradient"></div>
       <div className="circular-gradient"></div>
       <div className="circular-gradient"></div>
@@ -118,13 +116,14 @@ function TitlePage() {
         animate={{ scale: 1, opacity: 1 }}
         transition={{ duration: 2, ease: "easeInOut" }}
       />
+      {/* <img className="logo"  src={Logo} alt=""/> */}
       <motion.h1
         className="site-title"
         initial={{ y: -50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, ease: "easeOut" }}
       >
-        Jones Consortium | Jones Realty ventures
+        Jones Consortium <span className="pipe">|</span>Jones Realty ventures
       </motion.h1>
       <motion.p
         className="coming-soon-text"
@@ -132,7 +131,8 @@ function TitlePage() {
         animate={{ x: 0, opacity: 1 }}
         transition={{ delay: 0.5, duration: 1, ease: "easeInOut" }}
       >
-        A thriving vision is taking shape. Get ready to experience the extraordinary!
+        A thriving vision is taking shape. Get ready to experience the
+        extraordinary!
       </motion.p>
       <div className="progress-bar-container">
         <h1 className="progress-bar-text">Under Construction</h1>
@@ -148,6 +148,25 @@ function TitlePage() {
           <ContactCard platform={platform2} />
         </div>
       </div>
+      <div class="map-container">
+        <a
+          href="https://www.google.com/maps?q=12.955201,80.171195"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div class="map-rectangle">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3888.260102646029!2d80.16861967507617!3d12.955201087358668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDU3JzE4LjciTiA4MMKwMTAnMTYuMyJF!5e0!3m2!1sen!2sin!4v1736067221480!5m2!1sen!2sin"
+              width="600"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </a>
+      </div>
+      <footer>© 2025 All Rights Reserved</footer>
     </div>
   );
 }
